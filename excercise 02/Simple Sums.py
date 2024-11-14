3a142e01615bce88e30c9d9e048600b8f892a43e
muhammadmunimyasir <munimyasir.07@gmail.com>
2024-11-14 11:42:22 +0400

    Simple sums

---
+++ excercise 02/Simple Sums.py
@@ -0,0 +1,16 @@
+"""In this exercise, you will create and work with integer variables, perform arithmetic operations, and print the result to the console.
+Steps:
+1. Declare a variable and initialize it with the integer value 8.
+2. Declare a second variable and initialize it with the integer value 10.
+3. Declare a third variable that stores the sum of first two numbers.
+4. Print the value of the sum to the console
+"""
+y = 8
+x = 10
+z = x + y
+print(z)
+
+#Give y the value 8
+#Let x be equal to 10
+#Store the sum of x and y in variable z
+#The expected result z is 18
