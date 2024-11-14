153149741d76d9d25a60be7408c808503221d467
muhammadmunimyasir <munimyasir.07@gmail.com>
2024-11-14 11:42:33 +0400

    Biography

---
+++ excercise 03/Biography.py
@@ -0,0 +1,21 @@
+"""In this exercise, you’ll create a program that stores and prints your name, hometown, and age to the console using a Python dictionary.
+Steps:
+1. Store the information (name, hometown, and age) as key-value pairs in a
+dictionary.
+2. Print the values on separate lines using a single print() statement.
+3. Use variables with appropriate data types for each piece of information.
+"""
+
+name = ("muhammad munim")
+hometown = ("larkana")
+age = ("18")
+print("name:" , name , "hometown:" , hometown , "age;", age )
+
+#Assign the string "Muhammad munim" to the variable codename
+#Codename – Muhammad munim
+
+#That is why we will use the string “Larkana” when defining the variable of hometown.
+
+#Now set the string “18” to the desired variable, let’s assign it to the variable “age”.
+
+#Using the codenames print their home town and age
