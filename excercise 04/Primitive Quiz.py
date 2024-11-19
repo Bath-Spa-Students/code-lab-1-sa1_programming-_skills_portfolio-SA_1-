928b4acbab9884d14b0463d13b1041d0d0cd01e2
muhammadmunimyasir <munimyasir.07@gmail.com>
2024-11-19 12:58:55 +0400

    primitive quiz

---
+++ excercise 04/Primitive Quiz.py
@@ -0,0 +1,27 @@
+"""In this exercise, you’ll create a simple program that asks the user a question,
+evaluates their answer, and provides feedback.
+Steps:
+1. Write a program that asks the user “What is the capital of France?” and
+waits for their response.
+2. If the user’s answer is correct (i.e., “Paris”), the program should print a
+message saying the answer is correct.
+3. If the answer is incorrect, the program should print a message saying the
+answer is wrong.
+"""
+question = ("what is the capital of Afganistan")
+answer = input("Qabool")
+if answer == 'Qabool' or answer == 'qabool':
+    print("correct")
+else:
+    print("incorrect")
+    
+
+#Saves the question text in the variable called question.
+
+#Learns from the user what the question is by using the present question.
+
+#Takes a check in the format of an array, and compares the user answer with “Qabool” or “qabool”.
+
+#If the answer matches prints “correct”.
+
+#Performs the following line if the answer doesn’t match.
