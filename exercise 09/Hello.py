f59a9a496d4976f9622c15ca572cd11faec29c9c
muhammadmunimyasir <munimyasir.07@gmail.com>
2024-11-21 09:14:28 +0400

    hello

---
+++ exercise 09/Hello.py
@@ -0,0 +1,18 @@
+"""Fill in the blanks in the code below so that the function hello() prints “Hello”
+to the console.
+"""
+
+def hello():
+    print('Hello')
+    
+#call the function hello()
+hello()
+
+#define a function names 'hello'
+def hello ():
+    #print the greeting 'hello' when the function is called
+    print('hello')
+    
+    #call the function to execute its code
+    hello()
+
