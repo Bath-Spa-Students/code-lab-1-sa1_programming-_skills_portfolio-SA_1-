e03d3815e245dc0832defd894eb8d2cf428c6b00
muhammadmunimyasir <munimyasir.07@gmail.com>
2024-11-26 10:17:59 +0400

    class activities

---
+++ F_class activites/lists.py
@@ -0,0 +1,6 @@
+word1 = "Munim"
+word2 = "is"
+word3 = "here"
+
+result = word1 + " " + word2 + " " + word3 #This will print munim is here 
+print(result)
---
+++ F_class activites/practice.py
@@ -0,0 +1,48 @@
+print("Welcom to my code lab -1")
+#WELCOM MSG
+print("Muhammad Munim")
+print("im 17")
+print("i study at bsu")
+print("i like cars")
+print("i live in sharjah")
+print("i am from pakistan")
+
+print ("""Twinklw twinkle littile star
+                how i wondr what you are""")
+print ("""Andy wala burger 
+             khao piyo mazy karo""")
+print (""" suna tha har cheez mil jati hai dua se
+                  Phir vo kyu nahi mila jise manga tha khuda se""")
+print ("""Tumhe may apne lafzo me itna khoobsurat likhuga
+                   Padhne walon ko tumhe dekhne ki chaah hogi""")
+print ("""kaise bataon kyu tujhe ko chahu 
+          yara bata na paoun batian dilo ki dekho jo batain ake tujhe samjhao""")
+print ("""Bole churiya bole kangana 
+             haye mai hogy teri sajana""")
+x=10
+y=15
+print(x+y)
+#variables
+my_age = 17
+A=16
+print(my_age)
+print (my_age , A)
+
+A= 17
+B= 16
+C= 20
+print(A+B+C)
+
+A= 20
+B= "STUDENTS"
+print (A+B)
+
+#String Concatenation through the variable
+message = 'Muhammad' + 'Munim'
+print (message)
+
+#String Concatenation print
+print ('enter th amount of ' + ' sales for each day and ' + 'press enter.') 
+
+
+
