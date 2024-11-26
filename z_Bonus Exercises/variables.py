f7590027ca066243aa865299260cc2d9098b1e91
muhammadmunimyasir <munimyasir.07@gmail.com>
2024-11-26 09:55:25 +0400

    variables.py

---
+++ z_Bonus Exercises/variables.py
@@ -0,0 +1,11 @@
+"""Assign a message to a variable, and print that message. 
+Then change the value of the variable to a new message, and print the new message
+"""
+
+#assign the first message to the variable 
+message = "this is the first message."
+print(message)
+
+#change the value of the variable to new message
+message = "this is the updated message."
+print (message)
